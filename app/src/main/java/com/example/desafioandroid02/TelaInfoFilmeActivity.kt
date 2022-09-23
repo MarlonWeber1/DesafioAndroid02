3f670d1c6116c205e94689acbbc410f5a5dba7bf
MarlonWeber1 <marlonjlweberf@gmail.com>
2022-09-23 16:27:18 -0300

    commit 11
    Arruma tela de infos de filme

--- app/src/main/java/com/example/desafioandroid02/TelaInfoFilmeActivity.kt
+++ app/src/main/java/com/example/desafioandroid02/TelaInfoFilmeActivity.kt
@@ -2,6 +2,7 @@ package com.example.desafioandroid02
 
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.desafioandroid02.model.Filme
@@ -14,7 +15,13 @@ class TelaInfoFilmeActivity : AppCompatActivity() {
 
         var filme = intent.getSerializableExtra("filme") as Filme
         val imagem = findViewById<ImageView>(R.id.bannerFilme)
-        Glide.with(imagem).load("https://image.tmdb.org/t/p/w500${filme.bannerImagem}").into(imagem)
+        Glide.with(imagem).load("https://image.tmdb.org/t/p/w500${filme.posterImagem}").into(imagem)
+        val titulo = findViewById<TextView>(R.id.bindingNomeFilme)
+        titulo.text = filme.titulo
+        val dataLancamento = findViewById<TextView>(R.id.bindingDataLancamento)
+        dataLancamento.text = filme.dataDeLancamento
+        val descricao = findViewById<TextView>(R.id.bindingDescricao)
+        descricao.text = filme.descricao
 
     }
 }
