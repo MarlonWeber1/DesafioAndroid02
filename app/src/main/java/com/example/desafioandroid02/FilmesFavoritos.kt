14b3ac391057ae4f55f458c64d5b346d463c602c
MarlonWeber1 <marlonjlweberf@gmail.com>
2022-10-13 21:44:06 -0300

    commit 16
    room e database

--- app/src/main/java/com/example/desafioandroid02/FilmesFavoritos.kt
+++ app/src/main/java/com/example/desafioandroid02/FilmesFavoritos.kt
@@ -1,19 +1,41 @@
 package com.example.desafioandroid02
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.desafioandroid02.adapter.ListaFilmesAdapter
+import com.example.desafioandroid02.dao.FilmeFavoritoDAO
+import com.example.desafioandroid02.database.FilmesDatabase
 import com.example.desafioandroid02.databinding.ActivityListaFilmesBinding
+import com.example.desafioandroid02.model.Filme
 
 class FilmesFavoritos : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityListaFilmesBinding.inflate(layoutInflater)
     }
+    private lateinit var dao: FilmeFavoritoDAO
+    private var arrayList: ArrayList<Filme> = ArrayList()
+    private lateinit var recycler: RecyclerView
+    private val teste by lazy{
+        ListaFilmesAdapter();
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_filmes_favoritos)
+
+        dao = FilmesDatabase.FilmesDatabase.getInstance(this).getFilmeFavoritoDAO()
+
+        recycler = binding.activityListaNotasRecyclerview
+        recycler.adapter = teste
+        arrayList = dao.busca() as ArrayList<Filme>
+        teste.mandaProAdapter(dao.busca())
 
         binding.bottomNav.setOnItemSelectedListener {
             when(it.itemId){
@@ -29,4 +51,5 @@ class FilmesFavoritos : AppCompatActivity() {
             true
         }
     }
+
 }
--- app/src/main/java/com/example/desafioandroid02/TelaInfoFilmeActivity.kt
+++ app/src/main/java/com/example/desafioandroid02/TelaInfoFilmeActivity.kt
@@ -1,17 +1,28 @@
 package com.example.desafioandroid02
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
+import com.example.desafioandroid02.dao.FilmeFavoritoDAO
+import com.example.desafioandroid02.database.FilmesDatabase
 import com.example.desafioandroid02.model.Filme
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.launch
 
 class TelaInfoFilmeActivity : AppCompatActivity() {
+    private lateinit var dao: FilmeFavoritoDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_info_filme)
         setTitle("Info Filmes")
+
+        dao = FilmesDatabase.FilmesDatabase.getInstance(this).getFilmeFavoritoDAO()
 
         var filme = intent.getSerializableExtra("filme") as Filme
         val imagem = findViewById<ImageView>(R.id.bannerFilme)
@@ -24,6 +35,15 @@ class TelaInfoFilmeActivity : AppCompatActivity() {
         dataLancamento.text = filme.dataDeLancamento
         val descricao = findViewById<TextView>(R.id.bindingDescricao)
         descricao.text = filme.descricao
+
+        val favBotao = findViewById<FloatingActionButton>(R.id.botaoFilmeFavoritos)
+
+        favBotao.setOnClickListener(View.OnClickListener {
+            lifecycleScope.launch{
+                dao.salva(filme)
+                Toast.makeText(this@TelaInfoFilmeActivity, "Filme na tela de favoritos", Toast.LENGTH_SHORT).show()
+            }
+        })
 
     }
 }
